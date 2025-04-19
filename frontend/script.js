4c769a2b178b8d248ccd0755b99bbad37ffdd735
Vonna9 <yvonna820@gmail.com>
2025-04-19 12:27:40 +0000

    Added animation to title and paragraph that mentions verifying program. Added new header for the browser tab. Removed unecessary .png files. Added new font to index.html. All changes are reflected within the index.html and style.css files to ensure all final changes display the new webpage layout. font.css and Frontend.html were files that reflect design changes and ideas.

---
+++ frontend/script.js
@@ -0,0 +1,72 @@
+fetch('/api/hello')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('message').textContent = data.message;
+  });
+
+//fetch Groq api
+fetch('/api/groq', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ emailText: 'Test email message here.' })
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.message); // Should say: "Email received by backend!"
+  });
+
+  //email check
+  fetch('/api/check-email', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email: 'test@example.com' })
+  })
+    .then(res => res.json())
+    .then(data => console.log('Email check:', data.result));
+
+  //phone check
+  fetch('/api/check-phone', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ phone: '+14155552671' })
+  })
+    .then(res => res.json())
+    .then(data => console.log('Phone check:', data.result));
+
+  //url check
+  fetch('/api/check-url', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: 'http://phishingsite.com' })
+  })
+    .then(res => res.json())
+    .then(data => console.log('URL check:', data.result));
+
+
+
+  function displayForm() {
+    //hide all active form sections
+    document.getElementById('email-section').style.display = "none";
+    document.getElementById('text-section').style.display = "none";
+    document.getElementById('link-section').style.display = "none";
+
+    switch(document.getElementById('feature-select').value) {// unhides the section for the selected feature
+      case "select":
+        alert("Please select a feature from the dropdown menu");
+        break;
+      case "email":
+        document.getElementById('email-section').style.display = "block";
+        break;
+      case "text":
+        document.getElementById('text-section').style.display = "block";
+        break;
+      case "link":
+        document.getElementById('link-section').style.display = "block";
+    }
+  }
+
+
+
+  
