42ca2bce1d18ca01221720d1bfde7a88fc5eb0b6
Ian Rispin <ianrispin12@gmail.com>
2025-04-18 21:16:52 -0700

    Dynamically hide sections based on user selection
    Added an onclick function to the feature selection button that unhides the div that matches the user's feature selection in the dropdown menu, and hides all other sections. Also centered the title

--- frontend/script.js
+++ frontend/script.js
@@ -3,3 +3,24 @@ fetch('/api/hello')
   .then(data => {
     document.getElementById('message').textContent = data.message;
   });
+
+  function displayForm() {
+    //hide all active form sections
+    document.getElementById('email-section').style.display = "none";
+    document.getElementById('text-section').style.display = "none";
+    document.getElementById('link-section').style.display = "none";
+
+    switch(document.getElementById('feature-select').value) {// unhides the section for the selected feature
+      case "select":
+        alert("Please select a feature from the dropdown menu");
+        break;
+      case "email":
+        document.getElementById('email-section').style.display = "block";
+        break;
+      case "text":
+        document.getElementById('text-section').style.display = "block";
+        break;
+      case "link":
+        document.getElementById('link-section').style.display = "block";
+    }
+  }
